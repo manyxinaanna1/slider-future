e79b914d7062ecb32476b29c8d879b66dd21dc5e
Analitika6 <Analitika6@palata.by>
2024-03-20 16:37:21 +0300

    html, css, js

---
+++ js/script.js
@@ -0,0 +1,8 @@
+let buttonPrevios = document.querySelector('.button-previos');
+let buttonNext = document.querySelector('.button-next');
+let sliders = document.querySelector('.sliders');
+let currentSlide = 1;
+
+buttonNext.onclick = function() {
+    
+}
